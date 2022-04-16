45a7de118267960f8d9261eb352cefd9f69100ef
Narawich Saphimarn <narawichsaphimarn@gmail.com>
2022-04-16 20:59:52 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -6,14 +6,5 @@ RUN npm install -g npm@8.7.0
 RUN npm install && npm run build
 COPY . .
 
-ENV HOST=0.0.0.0
-ENV NODE_ENV=production
-ENV DATABASE_NAME=unisuncmsdevdb
-ENV DATABASE_USER=urquhmotrdhwqg
-ENV DATABASE_PASSWORD=efad4bb2169e67ddaa17c21aba5c76efc6a9daa6a06310949eba9a006bf258da
-ENV INSTANCE_CONNECTION_NAME=unisun-345117:asia-southeast1:unisun
-ENV BUCKET_NAME=unisun-345117.appspot.com
-ENV HOST_DB=45.91.135.224
-
 EXPOSE 1337
-CMD ["npm", "run", "start"]
+CMD ["npm", "start"]
--- config/env/development/database.js
+++ config/env/development/database.js
@@ -2,15 +2,13 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("HOST_DB"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "unisuncmsdevdb"),
-      user: env("DATABASE_USERNAME", "urquhmotrdhwqg"),
-      password: env(
-        "DATABASE_PASSWORD",
-        "efad4bb2169e67ddaa17c21aba5c76efc6a9daa6a06310949eba9a006bf258da"
-      ),
-      ssl: env.bool("DATABASE_SSL", false),
+      host: "45.91.135.224",
+      port: 5432,
+      database: "unisuncmsdevdb",
+      user: "urquhmotrdhwqg",
+      password:
+        "efad4bb2169e67ddaa17c21aba5c76efc6a9daa6a06310949eba9a006bf258da",
+      ssl: false,
     },
   },
 });
