7c89d8ece6e4124f94edcc5bec9717a2a62adb6e
Narawich.Saphimarn <NarawichSaphimarn@gmail.com>
2022-05-03 16:39:56 +0700

    remove project

--- src/api/advisor/controllers/advisor.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  advisor controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::advisor.advisor');
--- src/api/gather/controllers/gather.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  gather controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::gather.gather');
